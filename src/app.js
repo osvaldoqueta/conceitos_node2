5d0bb929ff82a8355f6d7328ed295efa538bec48
osvaldoqueta <osvaldoqueta@gmail.com>
2020-10-29 10:32:04 -0300

    Segundo-desafio-v2

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
   const express              = require("express");
 
-  const { v4, uuid, isUuid } = require('uuidv4');
+  const { uuid}               = require('uuidv4');
 
-  const cors = require("cors");
+  const cors                 = require("cors");
 
   // const { v4: uuid } = require('uuid');
 
@@ -14,45 +14,6 @@
 
   const repositories = [];
 
-  //middleware
-  function verify_like(request, response, next){
-
-    var { likes } = request.body;
-    
-    if(typeof likes == "number" && likes > 0){
-      return next();
-    }
-
-    response.status(400).json({"likes": likes}); 
-  }
-
-
-
-  function isUniqid(request, response, next){
-    
-    var { id } = request.params;
-
-    if(isUuid(id))
-      return next();
-    else
-      return response.status(400).json({'Error': 'ID não é um Uuid válido'});
-  }
-
-
-
-
-  function verify_ID(request, response, next){
-
-    var { id } = request.params;
-    
-    var repositories_id = repositories.findIndex(repository => repository.id === id);
-
-    if(repositories_id < 0){
-      return response.status(400).json({'Error': 'ID inválido', 'ID':repositories_id});  
-    }
-    return next();
-    
-  }
   
 
 
@@ -61,62 +22,77 @@
 
 
   app.get("/repositories", (request, response) => {
-    //var {title, url, techs, likes} = response.query;  
     
-    //repositories.push({id, title, url, techs, likes});
-
-    return response.status(200).json(repositories);
+    return response.json(repositories);
 
 
   });
 
   app.post("/repositories", (request, response) => {
     
-    var {title, url, techs} = request.body;
+    const {title, url, techs} = request.body;
     
-    const repository = {id:uuid(), title: title, url: url, techs:techs};
+    const repository = {
+      id:uuid(), 
+      title, 
+      url, 
+      techs, 
+      likes:0
+    };
     
     repositories.push(repository); 
 
-    return response.status(200).json(repositories); 
+    return response.json(repository); 
   });
 
 
 
-  app.put("/repositories/:id", isUniqid, (request, response) => {
+  app.put("/repositories/:id", (request, response) => {
 
-      var { id } = request.params;
+      const { id }               = request.params;
       //para
-      var {title, url, techs}  = request.body;
+      const {title, url, techs}  = request.body;
       
-      var repository_put       = {id:id, "title": title, "url": url, "techs":techs};
+      const id_positon           = repositories.findIndex(postion => postion.id === id);
       
-      const id_positon         = repositories.findIndex(postion => postion.id === id);
       
-      if(id_positon < 0){
-        return response.status(400).json({'Error': 'Não deu para atualizar'});
+      if(id_positon === -1) {
+        return response.status(400).json({error: 'não existe'})
       }
-      repositories[id_positon] = repository_put;
       
-      response.json(repositories);  
+      const repository_put       = {
+        id, 
+        title, 
+        url, 
+        techs, 
+        likes: repositories[id_positon].likes
+      };
+      
+      repositories[id_positon] = repository_put;
+    
+      return response.json(repository_put);  
            
   
   });
 
 
 
-  app.delete("/repositories/:id", isUniqid, (request, response) => {
+  app.delete("/repositories/:id", (request, response) => {
+    
+    const { id } = request.params;
 
-    var { id }       = request.params;
-    
-    const id_positon = repositories.findIndex(postion => postion.id === id);
-    
-    if(id_positon < 0){
-      return response.status(400).json({'Error': 'Invalid iD to delete'});
+    const findRepositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if(findRepositoryIndex >= 0) {
+
+      repositories.splice(findRepositoryIndex, 1);
+    }else{
+      return response.status(400).json({error: 'Repository does not exist'});
     }
-    repositories.splice(id_positon, 1);
 
-    return response.status(204).json({'msg':'deletado com successo'});
+    return response.status(204).send();
+
+    
     
   
   });
@@ -124,17 +100,19 @@
 
 
 
-  app.post("/repositories/:id/like", isUniqid, verify_like, (request, response) => {
+  app.post("/repositories/:id/like", (request, response) => {
     //
-    var { id }       = request.params;
-    //
-    var { likes }    = request.body;
+      const { id } = request.params;
 
-    var id_positon   = repositories.findIndex(postion => postion.id === id);
+      const findRepositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-    repositories[id_positon].likes = likes;        
+      if(findRepositoryIndex === -1) {
 
-    return response.json(repositories[id_positon]);
+        return response.status(400).json({error: 'Repository does not exist.'});
+      }
+      repositories[findRepositoryIndex].likes +=1;
+
+      return response.json(repositories[findRepositoryIndex]);
 
   });
 
